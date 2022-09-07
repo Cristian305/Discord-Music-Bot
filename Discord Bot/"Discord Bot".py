0832849501865e4ffc134140047aed6b1488cc4c
Cristian305 <66392760+Cristian305@users.noreply.github.com>
2022-09-07 19:59:51 -0400

    Add files via upload

---
+++ Discord Bot/"Discord Bot".py
@@ -0,0 +1,17 @@
+#Discord Bot
+import discord
+from discord.exe import commands
+import music
+
+client = discord.Client()
+#client.run['MTAxNzExNDQyOTQxMTgzNTk2Ng.GIR-od.SBm0YxZfPpTZTqYzxZgCdCeqBRRyB0zASJVPDs']
+
+muic = [music]
+
+client = commands.Bot(comand_prefix='/', intents = discord.INtents.all())
+
+for i in range(len(muic)):
+    muic[i].setup[client]
+
+client.run['MTAxNzExNDQyOTQxMTgzNTk2Ng.GIR-od.SBm0YxZfPpTZTqYzxZgCdCeqBRRyB0zASJVPDs']
+
---
+++ Discord Bot/music.py
@@ -0,0 +1,51 @@
+import discord
+from discord.exe import commands
+import youtube_dl
+
+class music(commands.Muic):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def join(self,ctx):
+        if ctx.author.voice is None:
+            await ctx.send('User is not in a VC')
+        voice_cahnnel = ctx.auhtor.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+            
+    @commands.command()
+    async def disconnect(self,ctx):
+        await ctx.voice_channel.diconnect()
+    
+    @commands.command()
+    async def play(self,ctx,url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streaamed 1-reconnect_delay_max 5', 'options': '-vn'}
+        YDL_OPTIONS = {'formats':'bestaudio'}
+        vc = ctx.voic_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+        vc.play(source)
+    
+    @commands.command()
+    async def pause(self,ctx):
+        await ctx.voice_client.pause()
+        await ctx.send('The music has been ⏸')
+
+    @commands.command()
+    async def resume(self,ctx):
+        await ctx.voice_client.resume()
+        await ctx.send('The music will continue playing')
+
+
+
+
+def setup(client):
+    client.add_muic(music(client))
+
